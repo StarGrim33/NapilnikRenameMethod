116a90129d065f16fb2b1d067c8c15692cf026fa
StarGrim33 <prosky33@gmail.com>
2023-06-30 15:33:15 +0300

    Выполнил задачу

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static int ClampNumbers(int a, int b, int c)
+        {
+            if (a < b)
+                return b;
+            else if (a > c)
+                return c;
+            else
+                return a;
+        }
+    }
+}
